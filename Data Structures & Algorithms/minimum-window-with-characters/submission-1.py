4fb8d3ad23663e0faf93e07f5c723d9a3c91182f
Jared Lueck <40571558+jaredlueck@users.noreply.github.com>
2026-06-24 09:37:09 -0400

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:        
+        window = {}
+
+        for char in t:
+            window[char] = window.get(char, 0) + 1
+
+        # cnt is the number of unique characters in the window
+        cnt = len(window)
+        res = ""
+
+        l = 0
+        # increasing the right pointer brings more characters into the window
+        for r in range(len(s)):
+            print(cnt)
+            if s[r] in window:
+                window[s[r]] -= 1
+                if window[s[r]] == 0:
+                    cnt -= 1
+
+            # the current window contains all the characters
+            # so try to shrink it by increasing the left pointer
+            while cnt == 0:
+                if not res or len(res) > r - l + 1:
+                    res = s[l:r+1]
+                if s[l] in window:
+                    window[s[l]] += 1
+                    if window[s[l]] > 0:
+                        cnt += 1
+                l += 1
+                print(s[l:r+1])
+                print((l, r, cnt))
+
+        return res
+                
+
+            
+
