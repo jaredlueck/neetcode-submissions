7d492446df54f2907536484e10c9d1db9e5172e9
Jared Lueck <40571558+jaredlueck@users.noreply.github.com>
2026-07-08 08:49:29 -0400

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(curr, opened, closed):
+            # no more paranthesis left
+            if opened == n and closed == n:
+                res.append(curr)
+                return
+            else:
+                # we can always place another opening paranthesis if we have more left
+                if opened < n:
+                    backtrack(curr + "(", opened + 1, closed)
+                # can only place a closing paranthesis if there is at least one unclosed opening
+                # paranthesis
+                if opened > closed:
+                    backtrack(curr + ")", opened, closed + 1)
+        backtrack("", 0, 0)
+        return res
